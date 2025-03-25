15ee1887bb666feb5a6b4ea2b55a121b57935cb2
Doujinx28 <doujinxnorbertkruz@gmail.com>
2025-03-25 17:38:04 +0100

    Configure helmet using parent helmen middleware

--- myApp.js
+++ myApp.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const app = express();
-#const helmet = require('helmet');
-#app.use(helmet.hidePoweredBy());
-#app.use(helmet.frameguard({action: 'deny'}));
-#app.use(helmet.xssFilter());
-#app.use(helmet.noSniff());
-#app.use(helmet.ieNoOpen());
-#const timeInSeconds = 90 * 24 * 60 * 60
-#app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
-#app.use(helmet.dnsPrefetchControl());
-#app.use(helmet.noCache());
-#app.use(helmet.contentSecurityPolicy({
-#	directives: {
-#	defaultSrc: ["'self'"],
-#	scriptSrc: ["'self'", 'trusted-cdn.com'],
-#	}
-#	})
-#);
+const helmet = require('helmet');
+//app.use(helmet.hidePoweredBy());
+//app.use(helmet.frameguard({action: 'deny'}));
+//app.use(helmet.xssFilter());
+//app.use(helmet.noSniff());
+//app.use(helmet.ieNoOpen());
+//const timeInSeconds = 90 * 24 * 60 * 60
+//app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
+//app.use(helmet.dnsPrefetchControl());
+//app.use(helmet.noCache());
+//app.use(helmet.contentSecurityPolicy({
+//	directives: {
+//	defaultSrc: ["'self'"],
+//	scriptSrc: ["'self'", 'trusted-cdn.com'],
+//	}
+//	})
+//);
 
 app.use(helmet({
 	contentSecurityPolicy: {
